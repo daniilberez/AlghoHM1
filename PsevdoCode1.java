cfc9992a16f65c7c3347187796e3a9cf6754c521
daniilberez <122410108+daniilberez@users.noreply.github.com>
2023-06-16 12:02:57 +0300

    Add files via upload

---
+++ PsevdoCode1.java
@@ -0,0 +1,19 @@
+package homework1;
+
+public class PsevdoCode1 {
+    public static void main(String[] args) {
+    int n1 = 3;
+    int n2 = 2;
+    int n3 = 1;
+        System.out.println(n1 + n2 + n3);
+    }
+}
+
+// псевдокод
+
+// START
+// READ первое число
+// READ второе число
+// READ третье число
+// WRITE вывести сумму трех чисел
+// END
